35737ebb9d79c8a80c31f8aa0a8d3c9f6909e60d
promecarus <haikalslipi@gmail.com>
2023-05-18 15:01:11 +0700

    rework sign up

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -9,41 +9,119 @@ class Auth extends BaseController
 {
     public function signup()
     {
-        return view('auth/signup', [
-            'title' => 'Sign Up'
+        return view("auth/signup", [
+            "title" => "Sign Up",
+            "bodyClass" => "hold-transition register-page",
+            "rules" => "{
+                email: {
+                    required: true,
+                    email: true,
+                    remote: {
+                        url: '/auth/checkEmailUnique',
+                        type: 'post',
+                        data: {email: function() {return $('#email').val()}}
+                    }
+                },
+                username: {
+                    required: true,
+                    minlength: 2,
+                    maxlength: 255,
+                    remote: {
+                        url: '/auth/checkUsernameUnique',
+                        type: 'post',
+                        data: {username: function() {return $('#username').val()}}
+                    }
+                },
+                password: {
+                    required: true,
+                    minlength: 6,
+                    maxlength: 255
+                },
+                confirm_password: {
+                    required: true,
+                    equalTo: '#password'
+                },
+                nama: {
+                    required: true,
+                    minlength: 2,
+                    maxlength: 255
+                }
+            }",
+            "messages" => "{
+                email: {
+                    required: 'Email tidak boleh kosong',
+                    email: 'Email tidak valid',
+                    remote: 'Email sudah terdaftar'
+                },
+                username: {
+                    required: 'Username tidak boleh kosong',
+                    minlength: 'Username minimal 2 karakter',
+                    maxlength: 'Username maksimal 255 karakter',
+                    remote: 'Username sudah terdaftar'
+                },
+                password: {
+                    required: 'Password tidak boleh kosong',
+                    minlength: 'Password minimal 6 karakter',
+                    maxlength: 'Password maksimal 255 karakter'
+                },
+                confirm_password: {
+                    required: 'Konfirmasi password tidak boleh kosong',
+                    equalTo: 'Konfirmasi password tidak sesuai dengan password'
+                },
+                nama: {
+                    required: 'Nama tidak boleh kosong',
+                    minlength: 'Nama minimal 2 karakter',
+                    maxlength: 'Nama maksimal 255 karakter'
+                }
+            }"
         ]);
+    }
+
+    public function checkEmailUnique()
+    {
+        return $this->response->setJSON(
+            (new UserModel())->where('email', $this->request->getVar('email'))->first()
+                ? false
+                : true
+        );
+    }
+
+    public function checkUsernameUnique()
+    {
+        return $this->response->setJSON(
+            (new UserModel())->where('username', $this->request->getVar('username'))->first()
+                ? false
+                : true
+        );
     }
 
     public function register()
     {
         $userModel = new UserModel();
-        $email = $this->request->getVar('email');
-        $username = $this->request->getVar('username');
-        $password = $this->request->getVar('password');
-        $confirm_password = $this->request->getVar('confirm_password');
-        $nama = $this->request->getVar('nama');
+        $email = $this->request->getVar("email");
+        $username = $this->request->getVar("username");
+        $password = $this->request->getVar("password");
+        $nama = $this->request->getVar("nama");
 
-        if (!$userModel->validate($this->request->getPost()) || $password != $confirm_password) {
-            $errors = $userModel->errors();
-
-            if ($password != $confirm_password) {
-                $errors['confirm_password'] = 'Konfirmasi password tidak sesuai';
-            }
-
+        if (empty($email) || empty($username) || empty($password) || empty($nama)) {
             return redirect()
-                ->to(base_url('auth/signup'))
+                ->to(base_url("auth/signup"))
                 ->withInput()
-                ->with('errors', $errors)
-                ->with('message_error', 'Registrasi gagal. Silakan coba lagi!');
-        } else {
-            $userModel->insert([
-                'email' => $email,
-                'username' => $username,
-                'password' => password_hash($password, PASSWORD_DEFAULT),
-                'nama' => $nama
-            ]);
-            return redirect()->to(base_url('auth/signin'))->with('message_success', 'Registrasi berhasil. Silakan login!');
+                ->with("message", "Harap isi semua field.")
+                ->with("type", "error");
         }
+
+        $userModel->insert([
+            "email" => $email,
+            "username" => $username,
+            "password" => password_hash($password, PASSWORD_DEFAULT),
+            "nama" => $nama
+        ]);
+
+        return redirect()
+            ->to(base_url("auth/signin"))
+            ->with("message", "Registrasi berhasil, silakan login!")
+            ->with("type", "success");
     }
 
     public function signin()
--- app/Views/auth/signup.php
+++ app/Views/auth/signup.php
@@ -1,31 +1,69 @@
-<?= $this->extend('app') ?>
+<?= $this->extend("app") ?>
 
-<?= $this->section('content') ?>
-<div class="flex justify-center items-center h-screen">
-    <div class="card shadow-lg p-6 w-96">
-        <h2 class="text-xl font-semibold mb-3">Sign Up</h2>
-        <form method="post" action="/auth/register" class="form-control w-full mb-3">
-            <input class="input input-bordered" name="email" placeholder="Email" type="text" value="<?= old('email') ?>">
-            <span class="label label-text-alt text-error"><?= session()->getFlashdata('errors')['email'] ?? '' ?></span>
+<?= $this->section("link") ?>
+<link rel="stylesheet" href="/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+<?= $this->endSection() ?>
 
-            <input class="input input-bordered" name="username" placeholder="Username" type="text" value="<?= old('username') ?>">
-            <span class="label label-text-alt text-error"><?= session()->getFlashdata('errors')['username'] ?? '' ?></span>
-
-            <input class="input input-bordered" name="password" placeholder="Password" type="password" value="<?= old('password') ?>">
-            <span class="label label-text-alt text-error"><?= session()->getFlashdata('errors')['password'] ?? '' ?></span>
-
-            <input class="input input-bordered" name="confirm_password" placeholder="Konfirmasi password" type="password" value="<?= old('confirm_password') ?>">
-            <span class="label label-text-alt text-error"><?= session()->getFlashdata('errors')['confirm_password'] ?? '' ?></span>
-
-            <input class="input input-bordered" name="nama" placeholder="Nama" type="text" value="<?= old('nama') ?>">
-            <span class="label label-text-alt text-error"><?= session()->getFlashdata('errors')['nama'] ?? '' ?></span>
-
-            <button class="btn w-full" type="submit">Submit</button>
-        </form>
-        <p class="text-center text-sm text-gray-500">
-            Sudah terdaftar?
-            <a href="/auth/signin" class="link link-info link-hover">Sign In</a>
-        </p>
+<?= $this->section("content") ?>
+<div class="register-box">
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a class="h1"><b><?= $_ENV["app.concept"] ?></b><?= $_ENV["app.theme"] ?></a>
+        </div>
+        <div class="card-body">
+            <form id="form" action="/auth/register" method="post">
+                <div class="input-group mb-3">
+                    <input id="email" type="email" name="email" class="form-control" placeholder="Email" value="<?= old('email') ?>">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input id="username" type="text" name="username" class="form-control" placeholder="Username" value="<?= old('username') ?>">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-at"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input id="password" type="password" name="password" class="form-control" placeholder="Password" value="<?= old('password') ?>">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" name="confirm_password" class="form-control" placeholder="Konfirmasi password" value="<?= old('confirm_password') ?>">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="text" name="nama" class="form-control" placeholder="Nama" value="<?= old('nama') ?>">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
+                    </div>
+                </div>
+            </form>
+            <p class="mb-0">
+                Sudah terdaftar?
+                <a href="/auth/signin" class="text-center">Sign In</a>
+            </p>
+        </div>
     </div>
 </div>
+<?= $this->include("component/formValidation") ?>
 <?= $this->endSection() ?>
