8fb215cc4bf9484af1f27f7b51c79b68f282622c
promecarus <haikalslipi@gmail.com>
2023-05-20 08:02:19 +0700

    add form table layout

--- app/Views/layout/dashboard.php
+++ app/Views/layout/dashboard.php
@@ -1,7 +1,12 @@
 <?= $this->extend("app") ?>
+
 <?= $this->section("link") ?>
+<link rel="stylesheet" href="/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+<link rel="stylesheet" href="/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
 <link rel="stylesheet" href="/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
 <?= $this->endSection() ?>
+
 <?= $this->section("content") ?>
 <div class="wrapper">
     <?= $this->include("component/navbar") ?>
@@ -24,5 +29,17 @@
         <?= $this->include("component/footer") ?>
     </div>
 </div>
+<script src="/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="/plugins/jszip/jszip.min.js"></script>
+<script src="/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <?= $this->endSection() ?>
---
+++ app/Views/layout/formTable.php
@@ -0,0 +1,53 @@
+<?= $this->extend('layout/dashboard') ?>
+
+<?= $this->section('content') ?>
+<div class="row">
+    <div class="col-12">
+        <div id="card-form" class="card">
+            <div class="card-header">
+                <h3 class="card-title">Form <?= $title ?></h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <form id="form" method="post">
+                    <div class="input-group">
+                        <input id="id" type="hidden" name="id">
+                    </div>
+                    <?= $this->renderSection("content") ?>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn btn-primary btn-block" onclick="storeData()">Store</button>
+                        </div>
+                        <div class="col">
+                            <button class="btn btn-warning btn-block" onclick="updateData()">Update</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <div id="card-table" class="card">
+            <div class="card-header">
+                <h3 class="card-title">Tabel <?= $title ?></h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <table id="table" class="table table-bordered table-striped"></table>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->include("component/formValidation") ?>
+<?= $this->include("component/formTableGenerator") ?>
+<?= $this->endSection() ?>
