3bbe84a2b27ba9c8462a59d44064f5f1436806b4
promecarus <haikalslipi@gmail.com>
2023-05-25 18:46:56 +0700

    update model role and user

--- app/Models/RoleModel.php
+++ app/Models/RoleModel.php
@@ -8,22 +8,22 @@ class RoleModel extends Model
 {
     protected $table = 'roles';
     protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $useSoftDeletes = true;
+    protected $protectFields = true;
+    protected $allowedFields = ['role'];
+
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 
     public function getRole($id = false)
     {
         if ($id == false) {
             return $this->findAll();
         }
-        return $this->where(['id' => $id])->first();
-    }
-
-    public function getRoleName($role_id)
-    {
-        return $this->where(['id' => $role_id])->first()['role'];
-    }
-
-    public function getRoleID($role)
-    {
-        return $this->where(['role' => $role])->first()['id'];
+        return $this->find($id);
     }
 }
